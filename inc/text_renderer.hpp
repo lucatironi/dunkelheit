67eabd5b7f57aee4c4a146ce88e347a915374c8f
Luca Tironi <luca.tironi@gmail.com>
2024-12-14 22:57:02 +0100

    Use fontSize in loading the glyphs and make them pixelated

--- inc/text_renderer.hpp
+++ inc/text_renderer.hpp
@@ -99,7 +99,7 @@ private:
         else
         {
             // set size to load glyphs as
-            FT_Set_Pixel_Sizes(face, 0, 8);
+            FT_Set_Pixel_Sizes(face, 0, fontSize);
 
             // disable byte-alignment restriction
             glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
@@ -131,8 +131,8 @@ private:
                 // set texture options
                 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
                 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
                 // now store character for later use
                 Character character = {
                     texture,
