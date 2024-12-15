c8bde9a2d87ad8b986679a0c8c9890bdc831f403
Luca Tironi <luca.tironi@gmail.com>
2024-12-15 23:44:21 +0100

    Refactor Shader class

--- inc/shader.hpp
+++ inc/shader.hpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <sstream>
 #include <iostream>
+#include <unordered_map>
 
 #include <glad/glad.h>
 #include <glm/glm.hpp>
@@ -13,151 +14,147 @@ class Shader
 {
 public:
     GLuint ID;
-    // constructor generates the shader on the fly
-    // ------------------------------------------------------------------------
-    Shader(const std::string vertexPath, const std::string fragmentPath)
+
+    // Constructor now accepts an optional geometry shader path
+    Shader(const std::string& vertexPath, const std::string& fragmentPath, const std::string& geometryPath = "")
     {
-        // 1. retrieve the vertex/fragment source code from filePath
-        std::string vertexCode;
-        std::string fragmentCode;
-        std::ifstream vShaderFile;
-        std::ifstream fShaderFile;
-        // ensure ifstream objects can throw exceptions:
-        vShaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
-        fShaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
+        std::string vertexCode, fragmentCode, geometryCode;
+
         try
         {
-            // open files
-            vShaderFile.open(vertexPath);
-            fShaderFile.open(fragmentPath);
-            std::stringstream vShaderStream, fShaderStream;
-            // read file's buffer contents into streams
-            vShaderStream << vShaderFile.rdbuf();
-            fShaderStream << fShaderFile.rdbuf();
-            // close file handlers
-            vShaderFile.close();
-            fShaderFile.close();
-            // convert stream into string
-            vertexCode = vShaderStream.str();
-            fragmentCode = fShaderStream.str();
+            // Read shader files
+            vertexCode = readFile(vertexPath);
+            fragmentCode = readFile(fragmentPath);
+            if (!geometryPath.empty())
+                geometryCode = readFile(geometryPath);
+
         }
-        catch (std::ifstream::failure& e)
+        catch (const std::ifstream::failure& e)
         {
-            std::cout << "ERROR::SHADER::FILE_NOT_SUCCESSFULLY_READ: " << e.what() << std::endl;
+            std::cerr << "ERROR::SHADER::FILE_NOT_SUCCESSFULLY_READ: " << e.what() << std::endl;
+            return;
         }
-        const char* vShaderCode = vertexCode.c_str();
-        const char* fShaderCode = fragmentCode.c_str();
-        // 2. compile shaders
-        GLuint vertex, fragment;
-        // vertex shader
-        vertex = glCreateShader(GL_VERTEX_SHADER);
-        glShaderSource(vertex, 1, &vShaderCode, NULL);
-        glCompileShader(vertex);
-        checkCompileErrors(vertex, "VERTEX");
-        // fragment Shader
-        fragment = glCreateShader(GL_FRAGMENT_SHADER);
-        glShaderSource(fragment, 1, &fShaderCode, NULL);
-        glCompileShader(fragment);
-        checkCompileErrors(fragment, "FRAGMENT");
-        // shader Program
+
+        GLuint vertex = compileShader(GL_VERTEX_SHADER, vertexCode);
+        GLuint fragment = compileShader(GL_FRAGMENT_SHADER, fragmentCode);
+
+        // Create shader program and link shaders
         ID = glCreateProgram();
         glAttachShader(ID, vertex);
         glAttachShader(ID, fragment);
+
+        // If a geometry shader is provided, compile and attach it
+        if (!geometryPath.empty())
+        {
+            GLuint geometry = compileShader(GL_GEOMETRY_SHADER, geometryCode);
+            glAttachShader(ID, geometry);
+            glDeleteShader(geometry);  // We can delete the geometry shader after linking
+        }
+
+        // Link the program and check for errors
         glLinkProgram(ID);
         checkCompileErrors(ID, "PROGRAM");
-        // delete the shaders as they're linked into our program now and no longer necessary
+
+        // Clean up shaders once they're linked
         glDeleteShader(vertex);
         glDeleteShader(fragment);
-
     }
-    // activate the shader
-    // ------------------------------------------------------------------------
-    void Use()
+
+    // Use the shader program
+    void Use() const
     {
         glUseProgram(ID);
     }
-    // utility uniform functions
-    // ------------------------------------------------------------------------
-    void SetBool(const std::string &name, bool value) const
-    {
-        glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
-    }
-    // ------------------------------------------------------------------------
-    void SetInt(const std::string &name, int value) const
-    {
-        glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
-    }
-    // ------------------------------------------------------------------------
-    void SetFloat(const std::string &name, float value) const
-    {
-        glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
-    }
-    // ------------------------------------------------------------------------
-    void SetVec2(const std::string &name, const glm::vec2 &value) const
-    {
-        glUniform2fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
-    }
-    void SetVec2(const std::string &name, float x, float y) const
-    {
-        glUniform2f(glGetUniformLocation(ID, name.c_str()), x, y);
-    }
-    // ------------------------------------------------------------------------
-    void SetVec3(const std::string &name, const glm::vec3 &value) const
-    {
-        glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
-    }
-    void SetVec3(const std::string &name, float x, float y, float z) const
-    {
-        glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z);
-    }
-    // ------------------------------------------------------------------------
-    void SetVec4(const std::string &name, const glm::vec4 &value) const
-    {
-        glUniform4fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
-    }
-    void SetVec4(const std::string &name, float x, float y, float z, float w)
-    {
-        glUniform4f(glGetUniformLocation(ID, name.c_str()), x, y, z, w);
-    }
-    // ------------------------------------------------------------------------
-    void SetMat2(const std::string &name, const glm::mat2 &mat) const
-    {
-        glUniformMatrix2fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(mat));
-    }
-    // ------------------------------------------------------------------------
-    void SetMat3(const std::string &name, const glm::mat3 &mat) const
-    {
-        glUniformMatrix3fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(mat));
-    }
-    // ------------------------------------------------------------------------
-    void SetMat4(const std::string &name, const glm::mat4 &mat) const
-    {
-        glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(mat));
-    }
+
+    // Setters for uniforms
+    void SetBool(const std::string& name, bool value) const { setUniform(name, static_cast<int>(value)); }
+    void SetInt(const std::string& name, int value) const { setUniform(name, value); }
+    void SetFloat(const std::string& name, float value) const { setUniform(name, value); }
+    void SetVec2(const std::string& name, const glm::vec2& value) const { setUniform(name, value); }
+    void SetVec3(const std::string& name, const glm::vec3& value) const { setUniform(name, value); }
+    void SetVec4(const std::string& name, const glm::vec4& value) const { setUniform(name, value); }
+    void SetMat2(const std::string& name, const glm::mat2& mat) const { setUniform(name, mat); }
+    void SetMat3(const std::string& name, const glm::mat3& mat) const { setUniform(name, mat); }
+    void SetMat4(const std::string& name, const glm::mat4& mat) const { setUniform(name, mat); }
 
 private:
-    // utility function for checking shader compilation/linking errors.
-    // ------------------------------------------------------------------------
-    void checkCompileErrors(GLuint shader, std::string type)
+    mutable std::unordered_map<std::string, GLint> uniformLocations;
+
+    // Function to read file into a string
+    std::string readFile(const std::string& path)
+    {
+        std::ifstream file(path);
+        if (!file)
+            throw std::ifstream::failure("Failed to open file: " + path);
+
+        std::stringstream stream;
+        stream << file.rdbuf();
+        return stream.str();
+    }
+
+    // Function to compile a shader
+    GLuint compileShader(GLenum type, const std::string& source)
+    {
+        GLuint shader = glCreateShader(type);
+        const char* code = source.c_str();
+        glShaderSource(shader, 1, &code, nullptr);
+        glCompileShader(shader);
+        checkCompileErrors(shader, type == GL_VERTEX_SHADER ? "VERTEX" : type == GL_FRAGMENT_SHADER ? "FRAGMENT" : "GEOMETRY");
+        return shader;
+    }
+
+    // Function to set uniforms (with caching of locations)
+    template<typename T>
+    void setUniform(const std::string& name, const T& value) const
+    {
+        GLint location = getUniformLocation(name);
+        setUniformImpl(location, value);
+    }
+
+    // Get uniform location and cache it
+    GLint getUniformLocation(const std::string& name) const
+    {
+        if (uniformLocations.find(name) != uniformLocations.end())
+            return uniformLocations[name];
+
+        GLint location = glGetUniformLocation(ID, name.c_str());
+        uniformLocations[name] = location;
+        return location;
+    }
+
+    // Template specialization for uniform setting based on type
+    void setUniformImpl(GLint location, bool value) const { glUniform1i(location, value); }
+    void setUniformImpl(GLint location, int value) const { glUniform1i(location, value); }
+    void setUniformImpl(GLint location, float value) const { glUniform1f(location, value); }
+    void setUniformImpl(GLint location, const glm::vec2& value) const { glUniform2fv(location, 1, glm::value_ptr(value)); }
+    void setUniformImpl(GLint location, const glm::vec3& value) const { glUniform3fv(location, 1, glm::value_ptr(value)); }
+    void setUniformImpl(GLint location, const glm::vec4& value) const { glUniform4fv(location, 1, glm::value_ptr(value)); }
+    void setUniformImpl(GLint location, const glm::mat2& mat) const { glUniformMatrix2fv(location, 1, GL_FALSE, glm::value_ptr(mat)); }
+    void setUniformImpl(GLint location, const glm::mat3& mat) const { glUniformMatrix3fv(location, 1, GL_FALSE, glm::value_ptr(mat)); }
+    void setUniformImpl(GLint location, const glm::mat4& mat) const { glUniformMatrix4fv(location, 1, GL_FALSE, glm::value_ptr(mat)); }
+
+    // Utility function to check compile/link errors
+    void checkCompileErrors(GLuint shader, const std::string& type) const
     {
         GLint success;
         GLchar infoLog[1024];
-        if(type != "PROGRAM")
+
+        if (type != "PROGRAM")
         {
             glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-            if(!success)
+            if (!success)
             {
-                glGetShaderInfoLog(shader, 1024, NULL, infoLog);
-                std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+                glGetShaderInfoLog(shader, 1024, nullptr, infoLog);
+                std::cerr << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog << std::endl;
             }
         }
         else
         {
             glGetProgramiv(shader, GL_LINK_STATUS, &success);
-            if(!success)
+            if (!success)
             {
-                glGetProgramInfoLog(shader, 1024, NULL, infoLog);
-                std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+                glGetProgramInfoLog(shader, 1024, nullptr, infoLog);
+                std::cerr << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << infoLog << std::endl;
             }
         }
     }
