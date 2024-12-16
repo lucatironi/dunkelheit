f2cc162b40328c33bbc9bf86e60c2fad28a4c8e7
Luca Tironi <luca.tironi@gmail.com>
2024-12-16 14:24:16 +0100

    Switch texture min filter

--- inc/texture2D.hpp
+++ inc/texture2D.hpp
@@ -11,7 +11,7 @@ struct TextureParams
 {
     GLuint wrapS = GL_CLAMP_TO_EDGE;
     GLuint wrapT = GL_CLAMP_TO_EDGE;
-    GLuint filterMin = GL_NEAREST_MIPMAP_NEAREST;
+    GLuint filterMin = GL_NEAREST_MIPMAP_LINEAR;
     GLuint filterMax = GL_NEAREST;
 };
 
