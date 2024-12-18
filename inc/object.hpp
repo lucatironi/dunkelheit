7b2beb857511e9c9f2af79b8781256fb88d98986
Luca Tironi <luca.tironi@gmail.com>
2024-12-18 09:56:24 +0100

    Add Object class

---
+++ inc/object.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+
+#include "file_system.hpp"
+#include "cube_model.hpp"
+
+class Object
+{
+public:
+    Object(const glm::vec3 pos)
+        : position(pos)
+    {
+    }
+
+    void Draw(const Shader& shader)
+    {
+        shader.Use();
+        shader.SetMat4("model", glm::translate(glm::mat4(1.0f), position));
+        model.Draw();
+    }
+
+private:
+    CubeModel model;
+    glm::vec3 position;
+};
