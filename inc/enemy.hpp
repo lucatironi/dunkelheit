49419c9612b9d95ddc1695b6d895e04e1844c38e
Luca Tironi <luca.tironi@gmail.com>
2026-01-08 22:59:32 +0100

    Add animation blending to enemy

--- inc/enemy.hpp
+++ inc/enemy.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "animated_model.hpp"
+#include "audio_engine.hpp"
 #include "entity.hpp"
 #include "fps_camera.hpp"
 #include "model_loader.hpp"
@@ -13,44 +14,84 @@
 #include <memory>
 #include <string>
 
+enum class EnemyState {
+    IDLE,
+    CRAWL,
+    RUN,
+    SCREAM,
+    ATTACK
+};
+
 class Enemy : public Entity
 {
 public:
     Enemy(const std::string& modelPath, const glm::vec3 position, const float angleY, const glm::vec3 scaleFactor)
-          : position(position), scaleFactor(scaleFactor), angleY(angleY)
+          : position(position), scaleFactor(scaleFactor), angleY(angleY), currentState(EnemyState::IDLE)
     {
         enemyModel = std::make_unique<AnimatedModel>();
         ModelLoader& gltf = ModelLoader::GetInstance();
         gltf.LoadFromFile(modelPath, *enemyModel);
-        enemyModel->SetCurrentAnimation(1);
+        enemyModel->PlayAnimation("1_idle", 0.5f);
         blobShadow = std::make_unique<PlaneModel>("assets/blob_shadow.png");
+        sound = AudioEngine::GetInstance().AddEmitter("assets/gizmo.wav", position);
 
         currentRotation = glm::angleAxis(glm::radians(angleY), glm::vec3(0.0f, 1.0f, 0.0f));
         updateModelMatrix();
     }
 
+    ~Enemy()
+    {
+        if (sound)
+            ma_sound_stop(sound);
+    }
+
     void Update(const float deltaTime, const FPSCamera& camera)
     {
-        glm::quat correction = glm::angleAxis(glm::radians(180.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-        float moveSpeed = 2.0f;
-        float turnSpeed = 1.0f;
+        float distToPlayer = glm::distance(position, camera.Position);
 
-        glm::vec3 direction = camera.Position - position;
-        direction.y = 0.0f;
+        // State Transition Logic
+        switch (currentState) {
+            case EnemyState::IDLE:
+                if (distToPlayer < 7.0f)
+                {
+                    currentState = EnemyState::CRAWL;
+                    enemyModel->PlayAnimation("6_crawl_run", 0.5f);
+                }
+                break;
 
-        if (glm::length(direction) > 0.1f)
-        {
-            direction = glm::normalize(direction);
-            // position += direction * moveSpeed * deltaTime;
+            case EnemyState::CRAWL:
+                handleMovement(deltaTime, camera.Position, 3.5f);
 
-            glm::quat lookRot = glm::quatLookAt(direction, glm::vec3(0.0f, 1.0f, 0.0f));
-            glm::quat targetRot = lookRot * correction;
+                if (distToPlayer < 3.0f)
+                {
+                    currentState = EnemyState::SCREAM;
+                    enemyModel->PlayAnimation("3_scream", 0.5f);
+                }
+                else if (distToPlayer > 10.0f)
+                {
+                    currentState = EnemyState::IDLE;
+                    enemyModel->PlayAnimation("1_idle", 0.5f);
+                }
+                break;
 
-            currentRotation = glm::slerp(currentRotation, targetRot, turnSpeed * deltaTime);
+            case EnemyState::SCREAM:
+                // Face the player while attacking
+                handleMovement(deltaTime, camera.Position, 0.0f);
+
+                // Logic: If animation finished or player moved away, resume chase
+                if (distToPlayer > 3.5f)
+                {
+                    currentState = EnemyState::CRAWL;
+                    enemyModel->PlayAnimation("6_crawl_run", 0.5f);
+                }
+                break;
         }
 
         updateModelMatrix();
         enemyModel->UpdateAnimation(deltaTime);
+
+        if (sound)
+            ma_sound_set_position(sound, position.x, position.y, position.z);
     }
 
     void Draw(const Shader& shader) const override
@@ -94,6 +135,28 @@ private:
     glm::quat currentRotation;
     glm::mat4 modelMatrix;
     std::unique_ptr<PlaneModel> blobShadow;
+    EnemyState currentState;
+    ma_sound* sound = nullptr;
+
+    void handleMovement(float deltaTime, glm::vec3 targetPos, float speed) {
+        glm::quat correction = glm::angleAxis(glm::radians(180.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        float turnSpeed = 2.0f;
+
+        glm::vec3 direction = targetPos - position;
+        direction.y = 0.0f;
+
+        if (glm::length(direction) > 0.1f) {
+            direction = glm::normalize(direction);
+
+            // Move toward player
+            position += direction * speed * deltaTime;
+
+            // Rotation
+            glm::quat lookRot = glm::quatLookAt(direction, glm::vec3(0.0f, 1.0f, 0.0f));
+            glm::quat targetRot = lookRot * correction;
+            currentRotation = glm::slerp(currentRotation, targetRot, turnSpeed * deltaTime);
+        }
+    }
 
     void updateModelMatrix()
     {
